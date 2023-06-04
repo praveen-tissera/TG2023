2b81c46743c36678e8fce3836f728451feace6cf
praveen-tissera <praveen.tissera@gmail.com>
2023-06-04 13:06:28 +0530

    form example

---
+++ practiceCode/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
+        <input type="password" name="userpassword" placeholder="password">
+        <input type="submit" name="login" value="Login" >
+    </form>
+</body>
+</html>
