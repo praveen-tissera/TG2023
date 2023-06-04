25136311f82f7dec568d436a0d4a47ae14a8d9d0
Rexadrew <resham.iox@gmail.com>
2023-06-04 00:43:05 -0700

    form2

---
+++ practiceCode/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+        <input type="email" name="user email" value="example@gmail.com" placeholder="Email">
+        <input type="password" name="user password" placeholder="Password">
+        <input type="submit" name="login" value="Login">
+    </form>
+</body>
+</html>
