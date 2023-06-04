f336dce8c5bf758798fd5bb9b7cae87ee9f54cfe
ChenithaKumarasinghe <chenithakumarasinghe@gmail.com>
2023-06-04 13:06:28 +0530

    form example

---
+++ practiceCode/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="get">
+    <input type="email" name="email" placeholder="your email">
+    <input type="password"name="password" placeholder="your password">
+    <input type="submit" name="login" value="Login" >
+   
+    </form>
+</body>
+</html>
