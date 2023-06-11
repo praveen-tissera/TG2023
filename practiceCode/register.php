ed208c22a09553d3ef375bee0c6e08ee26a482b9
Rexadrew <resham.iox@gmail.com>
2023-06-11 00:19:28 -0700

    register

---
+++ practiceCode/register.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form>
+        <table border="1">
+            <tr>
+                <td colspan="2">Register Page</td>
+            </tr>
+            <tr>
+                <td>Name:</td>
+                <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                <td><input type="text" name="email"></td>
+            </tr>
+            <tr>
+                <td>Address:</td>
+                <td>
+                    <textarea name="address" cols="30" rows="10"></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td>
+                    <input type="submit" name="submit" value="Register">
+                </td>
+            </tr>
+        </table>
+    </form>
+
+</body>
+
+</html>
