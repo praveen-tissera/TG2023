695c9ec2ea21ae032631be9f1f1c57b35938da13
ChenithaKumarasinghe <chenithakumarasinghe@gmail.com>
2023-06-18 14:41:42 +0530

    new

--- practiceCode/register_submit.php
+++ practiceCode/register_submit.php
@@ -1,5 +1,23 @@
 <?php
 
 print_r($_POST);
+echo $_POST('Email');
+
+//connect to database
+$connection = mysqli_connect('127.0.0.1','root', '', 'user_db');
+if($connection){
+echo "Connected Succesfully";
+}else{
+    echo "Databse connection fail"
+}
+
+//insert data into the database
+//table name, insert query
+
+$query_string = ""
+
+echo $query_string;
+
+mysqli_query($connection, $query_string)
 
 ?>
