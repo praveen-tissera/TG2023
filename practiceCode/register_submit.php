c1bb65a2fedf1f5da7e64607533842368e311aaf
Thulana2006 <thulanabaji@gmail.com>
2023-06-18 02:20:49 -0700

    db connection established

--- practiceCode/register_submit.php
+++ practiceCode/register_submit.php
@@ -1,3 +1,17 @@
 <?php 
 print_r($_POST);
+
+$connection = mysqli_connect('localhost', 'root', '', 'user_db');
+
+if($connection){
+    echo '<br>connection established.';
+}
+
+$query_string = "INSERT INTO `user_details` (`ID`, `Name`, `Email`, `Password`, `Address`) 
+                 VALUES (NULL, '{$_POST['name']}', '{$_POST['email']}', '{$_POST['password']}', '{$_POST['address']}')";
+
+echo $query_string."<br>";
+
+mysqli_query($connection, $query_string);
+
 ?>
