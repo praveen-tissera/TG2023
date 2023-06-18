a0ebd0a8beeb3618155fb9dac3d1169b008f78cb
Your Name <sithike.niran@gmail.com>
2023-06-18 13:45:14 +0530

    hi

--- practiceCode/register.php
+++ practiceCode/register.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>registration form</title>
 </head>
 <body>
     <form action="register_submit.php" method="post">
