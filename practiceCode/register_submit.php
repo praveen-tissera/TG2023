83c656eea0cfc7a1c34555d7103cabc3a79cc5b4
OmerRamzi <omermramzi@outlook.com>
2023-06-25 14:16:11 +0530

    form submission

--- practiceCode/register_submit.php
+++ practiceCode/register_submit.php
@@ -12,10 +12,30 @@ if($connection){
 }
 // insert data into the database
 // table name,  insert query
-
-$query_string = "INSERT INTO `register_tbl` (`id`, `name`, `email`, `password`, `address`, `created_date`) VALUES (NULL, '{$_POST['name']}', '{$_POST['email']}', '{$_POST['password']}', '{$_POST['address']}', '2023-06-18')";
+$current_date = date('Y-m-d');
+$query_string = "INSERT INTO `register_tbl` (`id`, `name`, `email`, `password`, `address`, `created_date`) VALUES (NULL, '{$_POST['name']}', '{$_POST['email']}', '{$_POST['password']}', '{$_POST['address']}', '$current_date')";
 
 echo $query_string;
 
-mysqli_query($connection, $query_string);
+$result = mysqli_query($connection, $query_string);
+//if record s added that count will be return from the mysqli_affecte_rows query if its fail -1 or nothing will return 0
+
+if(mysqli_affected_rows($connection) == 1){
+    echo "records added successfully";
+
+    header('Location:register.php?message= record added successfully');
+
+}else if(mysqli_affected_rows($connection) == -1){
+    echo "error";
+
+    header('Location:register.php?message= error');
+
+
+}else if(mysqli_affected_rows($connection) == 0){
+    echo "nothing added into the database";
+
+    header('Location:register.php?message= nothing added successfully');
+
+
+}
 ?>
