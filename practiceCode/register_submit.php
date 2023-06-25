f985674378405500a97887bc89108fcec755e462
rexadrew <resham.iox@gmail.com>
2023-06-25 01:04:28 -0700

    current-date

--- practiceCode/register_submit.php
+++ practiceCode/register_submit.php
@@ -1,4 +1,21 @@
-<?php
+<?php 
 print_r($_POST);
+echo $_POST['email'];
 
+// connect to the databae
+// create a link to Database
+$connection = mysqli_connect('127.0.0.1','root', '', 'user_db');
+if($connection){
+ echo "Connected Successfully";
+}else{
+    echo "database connection fails";
+}
+// insert data into the database 
+// table name,  insert query
+$current_date = date('Y-m-d');
+$query_string = "INSERT INTO `register_tbl` (`id`, `name`, `email`, `password`, `address`, `created_date`) VALUES (NULL, '{$_POST['name']}', '{$_POST['email']}', '{$_POST['password']}', '{$_POST['address']}', '$current_date')";
+
+echo $query_string;
+
+mysqli_query($connection, $query_string);
 ?>
