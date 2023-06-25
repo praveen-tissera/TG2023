b4d79d2ba2af2e4c81af41e88833c51f7ea4f4e9
DinethFerenando <chalaniekurera@gmail.com>
2023-06-25 14:11:42 +0530

    push

--- practiceCode/register_submit.php
+++ practiceCode/register_submit.php
@@ -1,10 +1,9 @@
 <?php 
 print_r($_POST);
-echo $_POST["email"]
-
+echo $_POST["email"];
 // connect to the database
 // create a link to Database
-$connection = mysql_connect('127.0.0.1','root','','user_db');
+$connection = mysqli_connect('127.0.0.1','root','','user_db');
 if($connection){
     echo "Connected Successfully";
 }else{
@@ -12,10 +11,25 @@ if($connection){
 }
 // insert data into the database
 // table name, insert query
-
-$query_string = "INSERT INTO 'register_tbl' ('id', 'name', 'email', 'password', 'addres', 'created_date') VALUES (NUll, '{$_post['name']}', '{$_post['email']}', '{$_post['password']}', '{$_post['address']}', '2023-06-18')";
+$current_date = date('Y-m-d');
+$query_string = "INSERT INTO `register_tbl` (`id`, `name`, `email`, `password`, `address`, `created_date`) VALUES (NUll, '{$_POST['name']}', '{$_POST['email']}', '{$_POST['password']}', '{$_POST['address']}', '$current_date')";
 
 echo $query_string;
 
-mysqli_query($connection, $query_string);
+$result = mysqli_query($connection, $query_string);
+// if recorde added that count will be return from the mysqli_affected_rows query if its fail return -1 or nothing happen will return 0
+
+if(mysqli_affected_rows($connection) == 1){
+    echo"recorde added successfully";
+
+    header('Location:register.php?message= record added successfully');
+
+}else if(mysqli_affected_rows($connection) == -1){
+    echo"error";
+
+}else if(mqli_affected_rows($connection) == 0){
+    echo"nothing added into the database";
+}
+
+
 ?>
