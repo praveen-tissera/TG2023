659c2c96618229d5b8c929e90a6f9ccced9146fc
DinethFerenando <chalaniekurera@gmail.com>
2023-07-16 14:11:24 +0530

    created login feature

--- practiceCode/form.php
+++ practiceCode/form.php
@@ -7,6 +7,12 @@
     <title>Document</title>
 </head>
 <body>
+<?php
+    if(isset($_GET['message'])){
+        echo $_GET['message'];
+    }
+    
+    ?>
     <form action="form_submit.php" method="post">
         <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
         <input type="password" name="userpassword" placeholder="password">
