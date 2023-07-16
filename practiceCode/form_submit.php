566a7e3432096fbd3f7440b0052464c85de7c9e1
Hypersonic2000 <103803545+Hypersonic2000@users.noreply.github.com>
2023-07-16 14:09:08 +0530

    Added Login/Form Submit

--- practiceCode/form_submit.php
+++ practiceCode/form_submit.php
@@ -1,9 +1,32 @@
-<?php 
+<?php
 // print_r($_GET);
 print_r($_POST);
 
-if($_POST['userpassword'] == $_POST['retypepassword']){
+if ($_POST['userpassword'] == $_POST['retypepassword']) {
     echo "Password correct";
-}else{
+    //password is match
+    //pass email and password to the db and validate
+    $query = "SELECT COUNT(*) as userExist FROM `register_tbl` WHERE email = '{$_POST['useremail']}' && password = '{$_POST['userpassword']}' ";
+    // create a link to Database
+    $connection = mysqli_connect('127.0.0.1', 'root', '', 'user_db');
+    if ($connection) {
+        echo "Connected Successfully";
+    } else {
+        echo "database connection fails";
+    }
+    $result = mysqli_query($connection, $query);
+    //convert result into array form
+    $result_in_array = mysqli_fetch_assoc($result);
+    print_r($result_in_array);
+    //if user exist
+    if($result_in_array['userExist']==1){
+        
+    }else{
+        //redirect to login page
+        header('Location:form.php?message= email or password incorrect. try again');
+    }
+    
+
+} else {
     echo "Password mismatch please try again";
 }
