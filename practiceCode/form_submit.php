770da0b5f42b855c1af3836f41de3475e1c4b34d
Jennelle Fernando <64464766+Tat-coder@users.noreply.github.com>
2023-07-23 13:05:34 +0530

    changes

--- practiceCode/form_submit.php
+++ practiceCode/form_submit.php
@@ -4,6 +4,9 @@ print_r($_POST);
 
 if($_POST['userpassword'] == $_POST['retypepassword']){
     echo "Password correct";
+
 }else{
     echo "Password mismatch please try again";
 }
+// 
+$query="SELTECT COUNT(*) as UserExist FROM 'register_tbl' WHERE email='{$_POST['useremail']}' && password='$_POST['"
