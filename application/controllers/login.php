907c154460ae36a81f2987bdd2e5b422ba7eaec4
Hypersonic2000 <103803545+Hypersonic2000@users.noreply.github.com>
2023-08-13 14:13:21 +0530

    Work for today

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,16 +1,39 @@
-<?php 
-Class Login extends CI_Controller {
+<?php
+class Login extends CI_Controller
+{
 
-    public function __construct() {
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+    }
+    public function index()
+    {
+        $this->load->view('register');
+    }
 
     // userlogin method
 
-    public function userlogin($name){
+    public function userlogin($name)
+    {
         // logic need to create
         $data['name'] = $name;
         print_r($data);
-        $this->load->view('login',$data);
+        $this->load->view('login', $data);
+    }
+
+    public function registerSubmit()
+    {
+        $this->form_validation->set_rules('name', 'Username', 'required');
+        $this->form_validation->set_rules('email', 'Email', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_rules('address', 'Address', 'required');
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('register');
+        } else  {
+            echo "Sucess" ;
+            //$this->load->view('formsuccess');
+        }
     }
 }
---
+++ application/views/register.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="<?php echo base_url() . '/css/bootstrap.min.css' ?>">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Register New User</h1>
+                <?php echo validation_errors('<div class="alert alert-danger" >', '</div>'); ?>
+                <?php echo form_open('login/registerSubmit'); ?>
+                <table class="table">
+                    <tr>
+                        <td colspan="2">Register page</td>
+                    </tr>
+                    <tr>
+                        <td>Name</td>
+                        <td><input class="form-control" type="text" name="name"></td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td><input class="form-control" type="text" name="email"></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input class="form-control" type="password" name="password"></td>
+                    </tr>
+                    <tr>
+                        <td>Address</td>
+                        <td>
+                            <textarea class="form-control" name="address" cols="30" rows="10"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><input class="btn btn-primary" type="submit" name="submit" value="Register"></td>
+                    </tr>
+                </table>
+                <?php echo form_close(); ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
