982de49679de237e7e84234d528a0c585b29640f
ChenithaKumarasinghe <chenithakumarasinghe@gmail.com>
2023-08-13 13:18:47 +0530

    rfddwsxZ

--- application/views/register.php
+++ application/views/register.php
@@ -1,19 +1,91 @@
 <!DOCTYPE html>
+
 <html lang="en">
+
 <head>
+
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url().'/
-    css/boostrap.min.css' ?>">
+
+    <link rel="stylesheet" href="<?php echo base_url() . '/css/bootstrap.min.css' ?>">
+
     <title>Register</title>
+
 </head>
+
 <body>
+
     <div class="container">
+
         <div class="row">
-            <div class= col-12>
+
+            <div class="col-12">
+
                 <h1>Register New User</h1>
+
+                <form action="register_submit.php" method="post">
+
+                    <table class="table">
+
+
+
+                        <tr>
+
+                            <td>Name</td>
+
+                            <td><input type="text" name="name" class="form-control"></td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td>Email</td>
+
+                            <td><input type="text" name="email" class="form-control"></td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td>Password</td>
+
+                            <td><input type="password" name="password" class="form-control"></td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td>Address</td>
+
+                            <td>
+
+                                <textarea name="address" cols="30" rows="10" class="form-control"></textarea>
+
+                            </td>
+
+                        </tr>
+
+                        <tr>
+
+                            <td></td>
+
+                            <td><input type="submit" name="submit" value="Register" class="form-control"></td>
+
+                        </tr>
+
+
+
+                    </table>
+
+                </form>
+
             </div>
+
         </div>
+
     </div>
+
 </body>
+
 </html>
