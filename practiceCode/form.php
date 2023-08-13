0d7df7488ece2728d445b7fb8062941804fe11ea
praveen-tissera <praveen.tissera@gmail.com>
2023-08-13 12:29:01 +0530

    form update

--- practiceCode/form.php
+++ practiceCode/form.php
@@ -8,7 +8,14 @@
     <link rel="stylesheet" href="styles/css/bootstrap.min.css">
 </head>
 <body>
+<!-- bootstrap grid system layout -->
+
+
 <?php require_once('navigation.php') ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-12">
 <?php if(isset($_GET['message'])){ ?>
 <div class="alert alert-success" role="alert">
     <?php echo $_GET['message']; ?>
@@ -21,5 +28,12 @@
         <input type="password" name="retypepassword" placeholder="retype password">
         <input type="submit" name="login" value="Login" >
     </form>
+
+        </div>
+    </div>
+</div>
+
+
 </body>
 </html>
+
--- practiceCode/navigation.php
+++ practiceCode/navigation.php
@@ -1,19 +1,32 @@
-<nav>
-    <?php
-    session_start();
+<?php
+session_start();
+?>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+
+      <?php
+    
     if (!isset($_SESSION['userEmail'])) {
     ?>
-        <ul>
-            <li>Home</li>
-
-            <li><a href="form.php"> login</a></li>
-        </ul>
-
-    <?php } else { ?>
-        <ul>
-            <li>Home</li>
-            <li><a href="profile.php"> Profile</a></li>
-            <li><a href="logout.php"> Logout</a></li>
-        </ul>
-    <?php } ?>
+      <li class="nav-item">
+        <a class="nav-link" href="form.php">Login</a>
+      </li>
+      <?php } else { ?>
+        <li class="nav-item">
+        <a class="nav-link" href="logout.php">Logout`</a>
+      </li>
+      <?php } ?>
+    </ul>
+  </div>
 </nav>
+
+    
