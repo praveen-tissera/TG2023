ec98be13f7b134581dae0a718a9a473ca44c2888
Hypersonic2000 <103803545+Hypersonic2000@users.noreply.github.com>
2023-08-13 12:15:36 +0530

    Ensure upto date

--- practiceCode/form.php
+++ practiceCode/form.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,31 +7,19 @@
     <title>Document</title>
     <link rel="stylesheet" href="styles/css/bootstrap.min.css">
 </head>
-
 <body>
-    <!-- bootstrap grid system layout -->
+<?php require_once('navigation.php') ?>
+<?php if(isset($_GET['message'])){ ?>
+<div class="alert alert-success" role="alert">
+    <?php echo $_GET['message']; ?>
+</div>
+<?php }?>
     
-    <?php require_once('navigation.php') ?>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <?php if (isset($_GET['message'])) { ?>
-                    <div class="alert alert-success" role="alert">
-                        <?php echo $_GET['message']; ?>
-                    </div>
-                <?php } ?>
-
-                <form action="form_submit.php" method="post">
-                    <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
-                    <input type="password" name="userpassword" placeholder="password">
-                    <input type="password" name="retypepassword" placeholder="retype password">
-                    <input type="submit" name="login" value="Login">
-                </form>
-
-            </div>
-        </div>
-    </div>
-
+    <form action="form_submit.php" method="post">
+        <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
+        <input type="password" name="userpassword" placeholder="password">
+        <input type="password" name="retypepassword" placeholder="retype password">
+        <input type="submit" name="login" value="Login" >
+    </form>
 </body>
-
 </html>
