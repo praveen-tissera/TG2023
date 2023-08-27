ec0964e5f638814f63b18fd90302188ca7e3d009
Thulana2006 <thulanabaji@gmail.com>
2023-08-27 13:01:34 +0530

    Change

--- practiceCode/form.php
+++ practiceCode/form.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="form_submit.php" method="post">
-        <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
-        <input type="password" name="userpassword" placeholder="password">
-        <input type="password" name="retypepassword" placeholder="retype password">
-        <input type="submit" name="login" value="Login" >
-    </form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+
+        <link rel="stylesheet" href="../css/style.css">
+    </head>
+    <body>
+        <form action="form_submit.php" method="post">
+            <input type="email" name="useremail" value="example@gmail.com" placeholder="your email">
+            <input type="password" name="userpassword" placeholder="password">
+            <input type="password" name="retypepassword" placeholder="retype password">
+            <input type="submit" name="login" value="Login" >
+        </form>
+    </body>
 </html>
