3dd46339991eba53480c3e66ed08fb1361e46961
Hypersonic2000 <103803545+Hypersonic2000@users.noreply.github.com>
2023-09-24 12:07:24 +0530

    Actually Update

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,34 +1,47 @@
-<?php
+<?php 
 
-class User_model extends CI_Model
-{
+class User_model extends CI_Model{
 
     // method to register user
-    public function registerUser($data)
-    {
+    public function registerUser($data){
         // print_r($data);
         $query = $this->db->select('*')
-            ->where('email', $data['email'])
-            ->get('register_tbl');
-
+                ->where('email',$data['email'])
+                ->get('register_tbl');
+        
         // echo $query->num_rows();
-        if ($query->num_rows() == 1) {
+        if($query->num_rows() == 1){
             return false;
-        } else {
+        }else{
             $this->db->insert('register_tbl', $data);
             return true;
         }
+        
     }
-    public function loginCheck($data)
-    {
+    public function loginCheck($data){
         $condition = "email='{$data['email']}' && password='{$data['password']}'";
         $query = $this->db->select('*')
-            ->where($condition)
-            ->get('register_tbl');
+                        ->where($condition)
+                        ->get('register_tbl');
         echo $this->db->last_query();
-        if ($query->num_rows() == 1) {
+        // return $query->result();
+        if($query->num_rows() == 1){
             return true;
-        } else {
+        }else{
+            return false;
+        }
+    }
+
+    public function getUserData($data){
+        $condition = "email='{$data['email']}'";
+        $query = $this->db->select('*')
+                        ->where($condition)
+                        ->get('register_tbl');
+        echo $this->db->last_query();
+        // return $query->result();
+        if($query->num_rows() == 1){
+            return $query->result();
+        }else{
             return false;
         }
     }
